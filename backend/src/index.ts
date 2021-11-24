c6dd5fe3b2e51840a79085911c99d220a173324b
oxabz <legmatt0@gmail.com>
2021-11-24 17:30:15 +0100

    Moving application initialisation to a Promise to make it async. Adding postgres extension

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,18 @@
 import logger from './logger';
 import app from './app';
+import {Application} from './declarations';
 
-const port = app.get('port');
-const server = app.listen(port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+app.then((app:Application)=>{
+  const port = app.get('port');
+  const server = app.listen(port);
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+  server.on('listening', () =>
+    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  );
+});
+
+
--- backend/src/knex.ts
+++ backend/src/knex.ts
@@ -1,8 +1,10 @@
 import knex from 'knex';
 import { Application } from './declarations';
 
-export default function (app: Application): void {
+export default async function (app: Application): Promise<void> {
   const { client, connection } = app.get('postgres');
   const db = knex({ client, connection });
+  // Adding uuid extension to postgres so that the project can identifies n-uplet with v4 uuid
+  await db.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   app.set('knexClient', db);
 }
