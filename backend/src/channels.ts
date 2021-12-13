75a2f3daebaad823705ddd6037600adf51d3c226
oxabz <legmatt0@gmail.com>
2021-12-13 21:07:21 +0100

    adding user chanel

--- backend/src/channels.ts
+++ backend/src/channels.ts
@@ -1,5 +1,4 @@
 import '@feathersjs/transport-commons';
-import { HookContext } from '@feathersjs/feathers';
 import { Application } from './declarations';
 
 export default function(app: Application): void {
